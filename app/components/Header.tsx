0932c267b83ea1151c1c1bf29e6ef39c55d5fbc6
martinbolsnes <martinbolsones@gmail.com>
2025-02-04 19:28:31 +0100

    add avatar and edit bg

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,26 +2,39 @@ import Link from 'next/link';
 import { auth, signOut } from '../../auth';
 import { Button } from '@/components/ui/button';
 import { LogOut } from 'lucide-react';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 
 export default async function Header() {
   const session = await auth();
   return (
-    <div className='bg-background border-b border-border px-4 md:px-8 py-4 flex items-center justify-between sticky top-0 z-10'>
+    <div className='bg-primary border-b border-border px-4 md:px-8 py-4 flex items-center justify-between sticky top-0 z-10'>
       <Link href='/'>
-        <h1 className='font-mono text-xl'>Lista</h1>
+        <h1 className='font-mono text-2xl'>LISTA</h1>
       </Link>
 
       {session ? (
-        <form
-          action={async () => {
-            'use server';
-            await signOut();
-          }}
-        >
-          <Button variant='ghost' size='icon' type='submit'>
-            <LogOut />
-          </Button>
-        </form>
+        <div className='flex flex-row items-center space-x-2'>
+          <Avatar className='h-6 w-6'>
+            <AvatarImage
+              src={session.user.image || ''}
+              alt={session.user.name || ''}
+            />
+            <AvatarFallback>
+              {session.user.name?.charAt(0) || 'U'}
+            </AvatarFallback>
+          </Avatar>
+
+          <form
+            action={async () => {
+              'use server';
+              await signOut();
+            }}
+          >
+            <Button variant='ghost' size='icon' type='submit'>
+              <LogOut className='ml-2' />
+            </Button>
+          </form>
+        </div>
       ) : (
         <Link href='/signin'>
           <Button variant='outline'>Logg inn</Button>
